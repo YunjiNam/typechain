a44755b502e318ec4154b5d71af15b8bb4016c9d
yunjiNam <yj607252@gmail.com>
2020-07-26 02:01:16 +0900

    study: typescript study setting

---
+++ index.ts
@@ -0,0 +1,11 @@
+const name = "pin",
+  age = 25,
+  gender = "male";
+
+const sayHi = (name, age, gender?) => {
+  console.log(`hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age);
+
+export {};
